e857692d01a55bb9ce2f299d20e2ff7d94e629c1
Blackstar84 <carlosalmada84@gmail.com>
2023-01-16 21:47:19 -0300

    se soluciona lo del repetidor

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  //Primero Inicializo el state
+  state={
+    //En la variable name la declaro como un string vacío o como null
+    name: "",
+  };
+
+  //Este es el evento que se va a encargar de ejecutar cada vez que se escriba algo en el input
+  handlerOnChange = (e) => {
+    //en value vamos a guardar lo que escribamos en el input
+    const value = e.target.value;
+    //con esto vamos a setear en name lo que está en value a name
+    this.setState({
+      name:value,
+    });
+  };
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.handlerOnChange} />
+        <p className="repeater">{this.state.name}</p>
       </div>
     );
   }
